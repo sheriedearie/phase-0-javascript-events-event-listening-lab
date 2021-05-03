039842cb6ffa86710bff6d09a0f0c430799bccca
Sheridan <sheridanmaustad@gmail.com>
2021-05-03 03:25:22 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-function addingEventListener() {
+// const input = document.getElementById('input').addingEventListener('click');
+// input.addingEventListener('click', function() {
+//   alert('I was clicked!');
+// }); 
+// function clickAlert() {
+//     alert('I was clicked!');
+//   }
+  
+//   input.addingEventListener('click', clickAlert); 
+
+function addingEventListener(){
+  const element = document.getElementById ('input')
+  element.addEventListener('click', function(){
+    alert('I was clicked!');
+  })
 }
